6122921e49987221ecb32c9e1a516095c0d68bd2
Plarpoon <plarpoon.dev@gmail.com>
2020-09-20 13:11:03 +0200

    HttpClient Async calls

--- SourceCode/Scraper.cs
+++ SourceCode/Scraper.cs
@@ -1,12 +1,12 @@
 ﻿using HtmlAgilityPack;
 using System.Diagnostics;
-using System.Linq;
+using System.Net.Http;
 
 namespace BMAH_WoM.SourceCode
 {
     public class Scraper
     {
-        public void ScrapeData()
+        public async void ScrapeData()
         {
             string[] wowservers = new string[]
             {
@@ -34,43 +34,48 @@ namespace BMAH_WoM.SourceCode
                 "US-arthas"
             };
 
-            int TotalWoWservers = wowservers.Count();
-
-            for (int i = 0; i < TotalWoWservers; i++)
+            foreach (string wowserver in wowservers)
             /*cycle trough different WoW servers consider changing said cycle with another type that supports
              * an index (will be the same amount of supported servers) so that you can apply a retry extra
              * cycle if it fails during data retrieval*/
 
             {
-                string wowserver = wowservers[i];
-
                 var url = @"https://www.tradeskillmaster.com/black-market?realm=" + wowserver;
                 Debug.Print(wowserver);
 
-                HtmlWeb web = new HtmlWeb();
-                var htmlDoc = web.Load(url);
-                htmlDoc.OptionEmptyCollection = true;
-                var rows = htmlDoc.DocumentNode.SelectNodes("//tbody/tr");
+                HttpClient client = new HttpClient();
 
-                foreach (var row in rows)
+                using (var response = await client.GetAsync(url))
                 {
-                    var EmptyTSM = row.SelectSingleNode("td[1]").InnerText;
-                    if (EmptyTSM == "No results found.")
+                    using (var content = response.Content)
                     {
-                        Debug.Print("TSM has no data for this server at the moment, please try later!");
-                    }
-                    else
-                    {
-                        var ItemName = row.SelectSingleNode("td[1]/a[1]").Attributes["title"].Value;
-                        var CurrentBid = row.SelectSingleNode("td[2]").InnerText;
-                        var MinBid = row.SelectSingleNode("td[3]").InnerText;
-                        var TimeLeft = row.SelectSingleNode("td[4]").InnerText;
-                        var NBids = row.SelectSingleNode("td[5]").InnerText;
-                        var RealmMarket = row.SelectSingleNode("td[6]").InnerText;
-                        var GlobalMarket = row.SelectSingleNode("td[7]").InnerText;
-                        var RealmAHQty = row.SelectSingleNode("td[8]").InnerText;
+                        var result = await content.ReadAsStringAsync();
+                        var document = new HtmlDocument();
+                        document.LoadHtml(result);
 
-                        Debug.Print("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", ItemName, CurrentBid, MinBid, TimeLeft, NBids, RealmMarket, GlobalMarket, RealmAHQty);
+                        var rows = document.DocumentNode.SelectNodes("//tbody/tr");
+
+                        foreach (var row in rows)
+                        {
+                            var EmptyTSM = row.SelectSingleNode("td[1]").InnerText;
+                            if (EmptyTSM == "No results found.")
+                            {
+                                Debug.Print("TSM has no data for this server at the moment, please try later!");
+                            }
+                            else
+                            {
+                                var ItemName = row.SelectSingleNode("td[1]/a[1]").Attributes["title"].Value;
+                                var CurrentBid = row.SelectSingleNode("td[2]").InnerText;
+                                var MinBid = row.SelectSingleNode("td[3]").InnerText;
+                                var TimeLeft = row.SelectSingleNode("td[4]").InnerText;
+                                var NBids = row.SelectSingleNode("td[5]").InnerText;
+                                var RealmMarket = row.SelectSingleNode("td[6]").InnerText;
+                                var GlobalMarket = row.SelectSingleNode("td[7]").InnerText;
+                                var RealmAHQty = row.SelectSingleNode("td[8]").InnerText;
+
+                                Debug.Print("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", ItemName, CurrentBid, MinBid, TimeLeft, NBids, RealmMarket, GlobalMarket, RealmAHQty);
+                            }
+                        }
                     }
                 }
             }
